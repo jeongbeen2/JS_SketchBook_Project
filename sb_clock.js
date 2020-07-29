2d8d9327f76913f87f2dc651df333e3e76c9d2f1
yoonsangkwak <67727981+yoonsangkwak@users.noreply.github.com>
2020-07-29 15:25:26 +0900

    wip
    add music

--- sb_clock.js
+++ sb_clock.js
@@ -13,3 +13,39 @@ setInterval(() => {
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
 });
+
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerWidth - e.pageY * speed) / 100;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
+
+document.querySelector('.tree_btn').addEventListener('click', function() {
+  document.querySelector('.triangle').classList.toggle('active')
+  document.querySelector('.box').classList.toggle('active')
+});
+
+
+let audioFile = new Audio('audio/christmas.mp3')
+document.querySelector('#play-btn').addEventListener('click', function() {
+  var playBtn = document.getElementById('play-btn');
+  if (playBtn.classList.contains('pause')) {
+    audioFile.pause();
+    document.querySelector('#play-btn').classList.remove('pause');
+  } else {
+    audioFile.play();
+    document.querySelector('#play-btn').classList.add('pause');
+  }
+});
+
+
+document.querySelector('#stop-btn').addEventListener('click', function() {
+  audioFile.pause();
+  audioFile.currentTime = 0;
+});
