1086cbdf4edb5ccfce51c1ddf231ffd6228677bb
yoonsangkwak <67727981+yoonsangkwak@users.noreply.github.com>
2020-07-29 19:59:13 +0900

    wip
    wip

--- js/sb_audio.js
+++ js/sb_audio.js
@@ -1,27 +1,30 @@
 let audioFile = new Audio('audio/christmas.mp3')
 
 let playBtn = document.getElementById('play-btn');
-document.querySelector('#play-btn').addEventListener('click', function() {
-  if (playBtn.classList.contains('pause')) {
-    audioFile.pause();
-    document.querySelector('#play-btn').classList.remove('pause');
-  } else {
+
+// 재생버튼 누르면 일시정지버튼
+let pause = 1;
+document.querySelector('#play').addEventListener('click', function() {
+  if (pause % 2 == '1') {
+    document.getElementById('play').src="css/img/pause.png";
     audioFile.play();
-    document.querySelector('#play-btn').classList.add('pause');
+  } else if (pause % 2 == '0') {
+    document.getElementById('play').src="css/img/play.png";
+    audioFile.pause();
   }
-});
+  pause++;
+})
 
-
+// 정지버튼
 document.querySelector('#stop-btn').addEventListener('click', function() {
   audioFile.pause();
   audioFile.currentTime = 0;
-  if (playBtn.classList.contains('pause')) {
-    audioFile.pause();
-    audioFile.currentTime = 0;
-    document.querySelector('#play-btn').classList.remove('pause');
-  }
+  document.getElementById('play').src="css/img/play.png";
+  pause++;
 });
-  
+
+
+// 볼륨조절
 audioFile.volume = 0.5;
 document.querySelector('#volume-up').addEventListener('click', function() {
   if (audioFile.volume > 0.9) {
@@ -33,20 +36,23 @@ document.querySelector('#volume-up').addEventListener('click', function() {
 
 document.querySelector('#volume-down').addEventListener('click', function() {
   if (audioFile.volume < 0.1) {
-    alert("볼륨이 최소입니다!")
+    audioFile.volume = 0;
+    document.getElementById("vol").src="css/img/mute2.png";
+    mute++;
   } else {
     audioFile.volume -= 0.1;
   }
 });
 
-let count = 1;
+// 음소거버튼
+let mute = 1;
 document.querySelector('#volume').addEventListener('click', function() {
-  if (count % 2 == '1') {
+  if (mute % 2 == '1') {
     document.getElementById("vol").src="css/img/mute2.png";
     audioFile.volume = 0;
-  } else if (count % 2 == '0') {
+  } else if (mute % 2 == '0') {
     document.getElementById("vol").src="css/img/vol2.png";
     audioFile.volume = 0.5;
   }
-  count++;
+  mute++;
 })
