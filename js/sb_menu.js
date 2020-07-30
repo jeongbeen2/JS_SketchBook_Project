637d6807eeccd5d8e65c62c9121650a782c2c991
dhkim92 <92hu@kakao.com>
2020-07-30 15:45:00 +0900

    Update nav btn WIP
    Co-Authored-By: DongHu Kim <92hu@kakao.com>
    Co-Authored-By: jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
    Co-Authored-By: Yoonsang Kwak <67727981+yoonsangkwak@users.noreply.github.com>

--- js/sb_menu.js
+++ js/sb_menu.js
@@ -1,6 +1,12 @@
-'use strict';
+"use strict";
 
 document.querySelector(".members").addEventListener("click", function () {
-    document.querySelector(".member__box").classList.toggle("active");
-  });
-  
+  document.querySelector(".member__box").classList.toggle("active");
+});
+
+/* WIP!!!!!!!! */
+function btnCross() {
+  document.querySelector("#mtop").classList.toggle("changes-top");
+  document.querySelector("#mbot").classList.toggle("changes-bot");
+  document.querySelector(".xbox").classList.toggle("xbox-click");
+}
