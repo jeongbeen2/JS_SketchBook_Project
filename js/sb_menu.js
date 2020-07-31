d6a0cafc81046bb9c52ddc88d5a97ff0dc9a2f0b
Jeong-Been-Kim <jb2n@github.com>
2020-07-31 12:48:19 +0900

    code cutting

--- js/sb_menu.js
+++ js/sb_menu.js
@@ -1,28 +1,29 @@
 "use strict";
 
-
 /* MEMBER BOX EFFECT */
 let mem = 1;
 document.querySelector(".members").addEventListener("click", memberImage);
 function memberImage() {
   if (mem % 2 == "0") {
     document.querySelector(".member__box").classList.add("active");
-    document.querySelector(".member__box__box1").classList.add("showing1");
-    document.querySelector(".member__box__box2").classList.add("showing2");
-    document.querySelector(".member__box__box3").classList.add("showing3");
+    document.querySelector(".member__box__box").classList.add("showing");
   } else if (mem % 2 == "1") {
     document.querySelector(".member__box").classList.remove("active");
-    document.querySelector(".member__box__box1").classList.remove("showing1");
-    document.querySelector(".member__box__box2").classList.remove("showing2");
-    document.querySelector(".member__box__box3").classList.remove("showing3");
+    document.querySelector(".member__box__box").classList.remove("showing");
   }
   mem++;
 }
-document.querySelector(".xbox").addEventListener("click", memberImage);
-/* WHAT BOX EFFECT */ /* WIPWIPWIPWIPWIPWIPWIPWIPWIPWIPWIP */
-document.querySelector(".what").addEventListener("click", function(){
-  document.querySelector(".ico-gooc__column").classList.toggle("move");
-});
+document
+  .querySelector(".xbox")
+  .addEventListener(
+    "click",
+    memberImage
+  ); /* WIPWIPWIPWIPWIPWIPWIPWIPWIPWIPWIP */
+/* WHAT BOX EFFECT */ document
+  .querySelector(".what")
+  .addEventListener("click", function () {
+    document.querySelector(".ico-gooc__column").classList.toggle("move");
+  });
 
 /* WIP!!!!!!!! */
 function btnCross() {
@@ -35,4 +36,3 @@ function btnCross() {
   document.querySelector(".ico-md-2").classList.toggle("js-ico-md");
   document.querySelector(".titleName").classList.toggle("js-titleName");
 }
-
