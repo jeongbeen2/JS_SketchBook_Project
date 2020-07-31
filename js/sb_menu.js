e19c387469c6e2e8309561ab4ef6dd1a8c65106c
yoonsangkwak <67727981+yoonsangkwak@users.noreply.github.com>
2020-07-31 13:12:20 +0900

    update reset
    reset function

--- js/sb_menu.js
+++ js/sb_menu.js
@@ -4,35 +4,47 @@
 let mem = 1;
 document.querySelector(".members").addEventListener("click", memberImage);
 function memberImage() {
-  if (mem % 2 == "0") {
-    document.querySelector(".member__box").classList.add("active");
-    document.querySelector(".member__box__box").classList.add("showing");
-  } else if (mem % 2 == "1") {
-    document.querySelector(".member__box").classList.remove("active");
-    document.querySelector(".member__box__box").classList.remove("showing");
-  }
-  mem++;
+  document.querySelector(".member__box").classList.toggle("active");
+  document.querySelector(".member__box__box").classList.toggle("showing");
 }
-document
-  .querySelector(".xbox")
-  .addEventListener(
-    "click",
-    memberImage
-  ); /* WIPWIPWIPWIPWIPWIPWIPWIPWIPWIPWIP */
-/* WHAT BOX EFFECT */ document
-  .querySelector(".what")
-  .addEventListener("click", function () {
-    document.querySelector(".ico-gooc__column").classList.toggle("move");
-  });
+// document.querySelector(".xbox").addEventListener("click",memberImage); 
+/* WIPWIPWIPWIPWIPWIPWIPWIPWIPWIPWIP */
+/* WHAT BOX EFFECT */ 
+document.querySelector(".what").addEventListener("click", function () {
+  document.querySelector(".ico-gooc__column").classList.toggle("move");
+});
 
 /* WIP!!!!!!!! */
+let cross = 1;
 function btnCross() {
-  document.querySelector("#mtop").classList.toggle("changes-top");
-  document.querySelector("#mbot").classList.toggle("changes-bot");
-  document.querySelector(".xbox").classList.toggle("xbox-click");
-  document.querySelector(".menu").classList.toggle("js-menu");
-  document.querySelector(".hide-clock").classList.toggle("js-hide-clock");
-  document.querySelector(".ico-md-1").classList.toggle("js-ico-md");
-  document.querySelector(".ico-md-2").classList.toggle("js-ico-md");
-  document.querySelector(".titleName").classList.toggle("js-titleName");
+    document.querySelector("#mtop").classList.add("changes-top");
+    document.querySelector("#mbot").classList.add("changes-bot");
+    document.querySelector(".xbox").classList.add("xbox-click");
+    document.querySelector(".menu").classList.add("js-menu");
+    document.querySelector(".hide-clock").classList.add("js-hide-clock");
+    document.querySelector(".ico-md-1").classList.add("js-ico-md");
+    document.querySelector(".ico-md-2").classList.add("js-ico-md");
+    document.querySelector(".titleName").classList.add("js-titleName");
+    cross = 2;
+}
+document.querySelector(".xbox").addEventListener("click", function() {
+  if (cross == 1) {
+    btnCross();
+  } else if (cross == 2) {
+    reset();
+  }
+});
+
+function reset() {
+  document.querySelector("#mtop").classList.remove("changes-top");
+  document.querySelector("#mbot").classList.remove("changes-bot");
+  document.querySelector(".xbox").classList.remove("xbox-click");
+  document.querySelector(".menu").classList.remove("js-menu");
+  document.querySelector(".hide-clock").classList.remove("js-hide-clock");
+  document.querySelector(".ico-md-1").classList.remove("js-ico-md");
+  document.querySelector(".ico-md-2").classList.remove("js-ico-md");
+  document.querySelector(".titleName").classList.remove("js-titleName");
+  document.querySelector(".member__box").classList.remove("active");
+  document.querySelector(".member__box__box").classList.remove("showing");
+  cross = 1;
 }
