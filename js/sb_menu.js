457d7e6b59ad0f5fafe14bce979e89c7b0215662
yoonsangkwak <yunsang08@naver.com>
2020-08-03 21:09:34 +0900

    add scroll js

--- js/sb_menu.js
+++ js/sb_menu.js
@@ -101,31 +101,3 @@ function menuReset(){
   document.querySelector(".canvas__box").style.display = "none";
 }
 
-$(document).on("scroll", naviBar);
-function naviBar() {
-  let h = $(window).scrollTop();
-  if (h >= $(".js-listener-second").position().top) {
-    $(".header").addClass("js-scroll-header");
-    $(".ico-md-1").addClass("js-scroll-ico-md-1");
-    $(".ico-md-2").addClass("js-scroll-ico-md-2");
-    $(".ico-gooc").addClass("js-scroll-ico-gooc");
-    $(".hide-clock").addClass("change");
-    $(".titleName").addClass("js-scroll-titleName");
-    $(".menu__btn").addClass("js-scroll-menu__btn");
-    $(".js-menu").addClass("js-scroll-menu");
-    $(".xbox").addClass("js-scroll-xbox");
-    $(".music").addClass("js-scroll-music");
-  } else {
-    $(".header").removeClass("js-scroll-header");
-    $(".ico-md-1").removeClass("js-scroll-ico-md-1");
-    $(".ico-md-2").removeClass("js-scroll-ico-md-2");
-    $(".ico-gooc").removeClass("js-scroll-ico-gooc");
-    $(".hide-clock").removeClass("change");
-    $(".titleName").removeClass("js-scroll-titleName");
-    $(".menu__btn").removeClass("js-scroll-menu__btn");
-    $(".js-menu").removeClass("js-scroll-menu");
-    $(".xbox").removeClass("js-scroll-xbox");
-    $(".music").removeClass("js-scroll-music");
-  }
-}
-naviBar();
